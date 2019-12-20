3fc86b57c34c5f1602f601dca8bf95ebd5026bce
pontakornth <pontakorn61@gmail.com>
2019-12-20 17:50:27 +0700

    NO MORE

--- components/TodosContainer.tsx
+++ components/TodosContainer.tsx
@@ -1,1 +1,25 @@
-// import React from 'react'
+import React from 'react';
+
+
+interface TodoItem {
+    title: string;
+    isFinished: boolean;
+}
+
+type TodosList = Array<TodoItem>;
+interface TodosGroup {
+    name: string;
+    todosList: TodosList;
+}
+
+interface TodosContainerProps {
+    todosGroups: Array<TodosGroup>;
+}
+
+const TodosContainer = (props: TodosContainerProps) => (
+  <div className="Container">
+    {props ? 'ho' : 'ha '}
+  </div>
+);
+
+export default TodosContainer;
---
+++ tests/TodosContainer.test.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TodosContainer from '../components/TodosContainer';
+
+describe('App', () => {
+  it('can render properly', () => {
+    expect(true).toBeFalsy();
+  });
+});
