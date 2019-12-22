1c0b80488dfb7f2dbc614c526ce34c4aa2475a48
pontakornth <pontakorn61@gmail.com>
2019-12-22 13:41:07 +0700

    Fix bugs: Incorrect key and name

--- components/TodosContainer.tsx
+++ components/TodosContainer.tsx
@@ -24,7 +24,7 @@ const TodosContainer = () => {
       <input className="todoGroupName" value={groupName} onChange={changeName} />
       <button type="button" onClick={handleClick}>Add</button>
       {todosGroups.length > 0 ? todosGroups.map((x) => (
-        <TodosGroup name={x} />
+        <TodosGroup key={x.key} name={x.name} />
       )) : 'No todos group'}
     </div>
   );
