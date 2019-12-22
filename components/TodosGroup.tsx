e456400a3b67498f99aaee1a6c9db94033c94212
pontakornth <pontakorn61@gmail.com>
2019-12-22 13:27:05 +0700

    Implement todo group

--- components/TodosGroup.tsx
+++ components/TodosGroup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface TodosGroupProps {
     name: string;
@@ -10,10 +10,46 @@ export interface TodoItem {
     isCompleted: boolean;
 }
 
-const TodosGroup = ({ name }: TodosGroupProps) => (
-  <div className="todo-group">
-    {name}
-  </div>
-);
+
+const TodosGroup = ({ name }: TodosGroupProps) => {
+  const [todosItems, setTodosItems] = useState([]);
+  const [newTodoName, setNewTodoName] = useState('');
+  const addTodoItem = (newTodo: TodoItem): void => {
+    setTodosItems([...todosItems, newTodo]);
+  };
+  const handleNewTodoChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewTodoName(e.target.value);
+  };
+  const handleClick = (): void => {
+    addTodoItem({
+      name: newTodoName,
+      isCompleted: false,
+    });
+    setNewTodoName('');
+  };
+  const handleCheck = (index: number) => {
+    setTodosItems([...todosItems.slice(0, index - 1),
+      { ...todosItems[index], isCompleted: !todosItems[index].isCompleted },
+      todosItems.slice(index + 1)]);
+  };
+  return (
+    <div className="todo-group">
+      <h1>
+        {name}
+      </h1>
+      <ul>
+        {todosItems ? todosItems.map((x: TodoItem, index: number) => (
+          <li className="todoItem">
+            <input onChange={() => handleCheck(index)} checked={x.isCompleted} type="checkbox" name={x.name} />
+            <p>{x.name}</p>
+          </li>
+
+        )) : '' }
+      </ul>
+      <input type="text" value={newTodoName} onChange={handleNewTodoChange} />
+      <button type="button" onClick={handleClick}>Add todo</button>
+    </div>
+  );
+};
 
 export default TodosGroup;
